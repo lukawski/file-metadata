9c11b272d3be3604df5bf04cf706e167e305e170
Kacper Łukawski <ka.lukawski@gmail.com>
2017-03-04 00:38:45 +0100

    Everything working

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import routes from './routes/upload'
+import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/api', routes)
+app.use('*', (req, res) => res.sendFile(path.join(__dirname, '/view/index.html')))
 
 app.listen(port, () => console.log(`App listening at port ${port}`))
